1233be94d646ee6e9e4b73e6881054cab482890c
zbQAQ <1290368140@qq.com>
2020-10-22 17:19:05 +0800

    feat: :sparkles:  添加动画工具函数

--- static/src/components/searchBar/searchBar.tsx
+++ static/src/components/searchBar/searchBar.tsx
@@ -1,23 +1,63 @@
-import React, { useState, useEffect } from "react"
-import { timeFormat } from "@/util/common"
+import React, { useState, useEffect, useRef } from "react"
+import { timeFormat, animation } from "@/util/common"
 
+import "./searchBar.css"
+
+// function getRealStyleValue(el: HTMLElement, key: string) {
+//   let value = window.getComputedStyle(el, null).getPropertyValue(key)
+//   value = value.includes('px') ? value.replace("px", "") : value
+//   return Number(value)
+// }
+
+// function animation(el: HTMLElement | null, options: Record<string, string | number>, time: number) {
+//   if(!el) return;
+//   let curVal = 0
+//   const setStyle = (key: string, target: string) => {
+//     if()
+//     el.style[key] += getRealStyleValue(el, key) + target
+//   }
+//   for(let k in options) {
+//     debugger;
+//     curVal = getRealStyleValue(el, k)
+//     setStyle(k, (curVal - options[k]) / time + 'px')
+//   }
+// }
 interface IProps {
+  // 时钟功能的flag
   timerFlag?: Boolean
 }
 
 export default function SearchBar(props: IProps) {
-  const [timeText, setTimeText] = useState(timeFormat(new Date(), "HH:mm"));
+  const [timeText, setTimeText] = useState(timeFormat(new Date(), "HH:mm:ss"));
+  const testBox = useRef(null)
   const { timerFlag = true } = props
-  //计时功能
+  //时钟功能
   if(timerFlag) {
     setInterval(() => {
-      setTimeText(timeFormat(new Date(), "HH:mm"))
+      setTimeText(timeFormat(new Date(), "HH:mm:ss"))
     }, 1000)
+  }
+
+  useEffect(() => {
+    console.log("useEffect")
+  }, [])
+
+  const move = () => {
+    console.log("mooove", testBox)
+    animation(testBox.current, {
+      width: 200,
+      height: 500,
+      "font-size": 30
+    }, 5)
   }
 
   return (
     <div className="searchBar">
       <p className="countTime">{timeText}</p>
+
+      <div ref={testBox} className="testBox">testBox</div>
+
+      <button onClick={move}>click me</button>
     </div>
   )
 }
--- static/src/util/common.ts
+++ static/src/util/common.ts
@@ -26,3 +26,60 @@ export const timeFormat = (time: number | Date, fmt = "yyyy-MM-dd") => {
 
   return resTime;
 }
+
+export const getRealStyleValue = (el: HTMLElement, key: string) => {
+  return window.getComputedStyle(el, null).getPropertyValue(key)
+}
+
+/**
+ * @name animation 动画函数
+ * 
+ * @param el 元素HTMLElement
+ * @param options 样式修改项
+ * @param rate 速率 越大越慢越小越快
+ * 
+ * options 支持大部分基础样式
+ * 需要注意的是 opacity的范围是 1~100 而不是 0~1
+ * 
+ * key 不支持驼峰 比如 fontSize就不行 必须font-size
+ * 
+ */
+export const animation = (el: HTMLElement | null, options: Record<string,  number>, rate: number = 10) => {
+  if(!el) return;
+  // debugger;
+  const timer = setInterval(() => {
+    let flag = true;
+    for(let k in options) {
+      // debugger;
+      let curVal: number;
+      let realVal = getRealStyleValue(el, k)
+      let unit = ""
+      if(realVal.includes("px")) {
+        unit = "px"
+      }
+
+      if(k === 'opacity') {
+        curVal = Number(realVal) * 100
+      }else {
+        curVal = parseInt(realVal.replace("px", "")) //加上parseInt修复原始值带小数的情况
+      }
+
+      if(curVal !== options[k]) {
+        flag = false
+      }
+
+      let speed = (options[k] - curVal) / rate //速率
+      speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed)
+
+      if(k === 'opacity') {
+        el.style[k] = (curVal + speed) / 100 + unit
+      }else {
+        el.style[k] = curVal + speed + unit
+      }
+      
+    }
+    if(flag) {
+      clearInterval(timer)
+    }
+  }, 1000/60)
+}

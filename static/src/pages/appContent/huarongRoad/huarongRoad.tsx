e1821833c8fb75150649950d38417d58cecb08ab
zbQAQ <1290368140@qq.com>
2021-01-06 18:41:45 +0800

    feat:  完善huarongroad拖拽逻辑

--- static/src/pages/appContent/huarongRoad/huarongRoad.tsx
+++ static/src/pages/appContent/huarongRoad/huarongRoad.tsx
@@ -1,42 +1,192 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useCallback, useEffect, useRef, useState } from "react"
 
+interface IContainerInfo {
+  offsetTop: number  //div距离屏幕顶部的高度
+  offsetLeft: number //div距离屏幕左侧的宽度
+  scrollTop: number  //div滚动了多少
+}
+
+interface IMousePosition {
+  x: number  //div距离屏幕顶部的高度
+  y: number //div距离屏幕左侧的宽度
+}
 
 import "./huarongRoad.css"
 
 const capacity = { col: 8, row: 40 }
 
+const randomColor = () => {
+  return '#' + Math.random().toString(16).substr(2, 6).toUpperCase();
+}
+
+let goods: any = [
+  {
+    id: 1,
+    style: {
+      backgroundColor: 'rgb(87 80 204 / 0.8)',
+    },
+    size: { col: 2, row: 1},
+    coordinate: { x: 0, y: 0, angle: 0 }
+  },
+  {
+    id: 2,
+    style: {
+      backgroundColor: 'rgb(66 66 123 / 0.8)',
+    },
+    size: { col: 3, row: 4},
+    coordinate: { x: 0, y: 5, angle: 0 }
+  },
+]
+
+const goodsMockApi = {
+  findItemById: (id: number) => {
+    return goods.find((v: { id: number; }) => v.id === id)
+  },
+  changeCoordinate: (id: number, x: number, y: number, angle?: number) => {
+    const { coordinate } = goods.find((v: { id: number; }) => v.id === id)
+    if(coordinate.x === x && coordinate.y === y && coordinate.angle === angle) return;
+    coordinate.x = x
+    coordinate.y = y
+    coordinate.angle = angle
+    return goods
+  }
+}
+
+function getGridByMousePos(mousePos: IMousePosition, containerInfo: IContainerInfo) {
+
+  console.log("mousePos", mousePos)
+  console.log("containerInfo", containerInfo)
+
+}
+
+
 export default function HuarongRoad() {
+
+  const [isDragging, setIsDragging] = useState(false)
+  const [draggingId, setDraggingId] = useState(0)
+  const [mousePosition, setMousePosition] = useState<IMousePosition>({ x: 0, y: 0 })
+
+  const [containerInfo, setContainerInfo] = useState<IContainerInfo>({
+    offsetTop: 0,
+    offsetLeft: 0,
+    scrollTop: 0,
+  })
 
   let gridMap = Array(capacity.row).fill(Array(capacity.col).fill(''))
 
-  let goods: any = [
-    {
-      id: 1,
-      backgroundColor: 'red',
-      size: { col: 1, row: 1},
-      coordinate: { x: 0, y: 0, angle: 0}
+  const singleGridW = 100
+  const singleGridH = 100
+
+  useEffect(() => {
+    const container = document.getElementById("huarongContainer")
+    if(container) {
+      const { top, left } = container.getBoundingClientRect() as any
+      setContainerInfo({
+        offsetTop: top,
+        offsetLeft: left,
+        scrollTop: container.scrollTop
+      })
     }
-  ]
+  }, [])
+
+  useEffect(() => {
+    if(isDragging) {
+      window.addEventListener("mousemove", handleMouseMove)
+      window.addEventListener("mouseup", handleMouseUp)
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+  }, [isDragging])
+
+  const handleMouseDown = (e: any, id: number) => {
+    e.persist()
+    setIsDragging(true)
+    setDraggingId(id)
+    // setMousePosition({ x: e.clientX, y: e.clientY })
+  }
+
+  const handleMouseMove = (e: any) => {
+    if(isDragging && draggingId != 0) {
+      //拖拽中
+      console.log("拖拽中", { x: e.clientX, y: e.clientY })
+      setMousePosition({ x: e.clientX, y: e.clientY })
+    }
+  }
+  
+  const handleMouseUp = (e: any) => {
+    const hoverGoodsItem = goodsMockApi.findItemById(draggingId)
+    console.log(hoverGoodsItem)
+
+    getGridByMousePos(mousePosition, containerInfo)
+
+    setIsDragging(false)
+    setDraggingId(0)
+  }
 
   const renderGrid = () => {
     let html = []
     for(let i = 0; i < gridMap.length; i++) {
       const curRow = gridMap[i]
       for(let j = 0; j < curRow.length; j++) {
-        html.push(<div className="singleGrid fontWhite textCenter">{i}, {j}</div>)
+        html.push(<div className="singleGrid fontWhite textCenter" key={`${i},${j}`}>{i}, {j}</div>)
       }
     }
     return html
   }
 
   const renderGoods = () => {
+    let html = []
+    
+    for(let i = 0; i < goods.length; i++) {
+      let { id, coordinate, size, style } = goods[i]
+      const width = size.row * singleGridW
+      const height = size.col * singleGridH
+      let left = coordinate.x * singleGridW
+      let top = coordinate.y * singleGridH
+      if(isDragging && draggingId !== 0) {
+        left = draggingId === id ? mousePosition.x - containerInfo.offsetLeft - width / 2 : left
+        top = draggingId === id ? mousePosition.y - containerInfo.offsetTop - height / 2 + containerInfo.scrollTop : top
+      }
+      let styles = {
+        ...style,
+        left,
+        top,
+        width,
+        height,
+      }
+      html.push(
+        <div 
+          key={id}
+          className="goodsItem" 
+          style={styles}
+          onMouseDown={(e) => {handleMouseDown(e, id)}}
+        ></div>
+      )
+    }
+
+    return html
 
   }
   
+  const handleScroll = (e: any) => {
+    e.persist()
+    setContainerInfo({
+      ...containerInfo,
+      scrollTop: e.target.scrollTop
+    })
+  }
+
   const render = () => {
     return (
-      <div className="huarongContainer">
+      <div className="huarongContainer" id="huarongContainer" onScroll={handleScroll}>
         {renderGrid()}
+        {renderGoods()}
       </div>
     )
   }

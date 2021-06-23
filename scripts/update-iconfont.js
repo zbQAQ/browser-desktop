54fa2421d85e939e1c3e37dbf60d17a76a44fa57
zhouruibin <zhouruibin@tuzhanai.com>
2021-06-23 17:05:38 +0800

    feat: :sparkles:  添加命令时修改 iconfont 相关地址功能呢

---
+++ scripts/update-iconfont.js
@@ -0,0 +1,56 @@
+const inquirer = require("inquirer")
+const path = require("path")
+const fs = require('fs');
+
+const templateFilePath = path.resolve(__dirname, "../static/template.html")
+
+const opitons = [
+  {
+    name: "cssLink",
+    message: "please input iconfont css link",
+    type: "input"
+  },
+  {
+    name: "jsLink",
+    message: "please input iconfont js link",
+    type: "input"
+  },
+]
+
+const cssReg = /(?<=<!-- iconfont css start --><link rel="stylesheet" href=")(.*)(?=" \/><!-- iconfont css end -->)/g
+const jsReg = /(?<=<!-- iconfont js start --><script src=")(.*)(?="><\/script><!-- iconfont js end -->)/g
+
+inquirer.prompt(opitons)
+  .then(input => {
+    fs.readFile(templateFilePath, function(err, data) {
+      if(err) {
+        console.log(err);
+        return false;
+      }
+      let templateString = data.toString()
+
+      if(input.cssLink) {
+        templateString = templateString.replace(cssReg, input.cssLink)
+        console.log('\n templateString: iconfont cssLink replace successul')
+      }
+      
+      if(input.jsLink) {
+        templateString = templateString.replace(jsReg, input.jsLink)
+        console.log('\n templateString: iconfont jsLink replace successul')
+      }
+
+      if(templateString !== data.toString()) {
+        // 当内容有修改时才 写入文件
+        fs.writeFile(templateFilePath, templateString, function(err) {
+          if(err) {
+            return console.log(err);
+          }
+          console.log("\n The template.html write successul and save!!!");
+        });
+      }
+    });
+
+  })
+  .catch(errr => {
+    console.log("err", err);
+  })

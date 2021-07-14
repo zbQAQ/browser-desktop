a16ce3bdd7f96c4965c3a1893384c402ff133618
zhouruibin <zhouruibin@tuzhanai.com>
2021-07-14 15:56:01 +0800

    fix: 修复壁纸异常消失的问题

--- static/src/components/transitionGroup/transitionGroup.tsx
+++ static/src/components/transitionGroup/transitionGroup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useCallback } from "react"
+import React, { PropsWithChildren, useRef, useCallback } from "react"
 
 import "./transitionGroup.css"
 interface IProps {
@@ -17,13 +17,10 @@ interface IProps {
 
   className?: string;
 
-  children?: React.ReactNode
 }
 
-export default function TransitionGroup(props: IProps) {
+export default function TransitionGroup(props: PropsWithChildren<IProps>) {
   const { enterAnimation = "fadeIn", levaeAnimation = "fadeOut", visible, delay = 500, className = '' } = props
-  // 需要用新的标识用在 动画结束后 隐藏元素
-  const [ flag, setFlag ] = useState(visible)
   const transitionGroup = useRef<HTMLDivElement>(null);
 
   // const addEventListener = (el: HTMLDivElement, eventName: string, callBack: ()=>void) => {
@@ -35,25 +32,18 @@ export default function TransitionGroup(props: IProps) {
   //   el.removeEventListener(eventName, callBack)
   // }
 
-  useEffect(() => {
-    if(visible) {
-      setFlag(visible)
-    } else {
-      setTimeout(() => setFlag(visible), delay)
-    }
-  }, [visible])
-
-  const render = () => {
+  const render = useCallback(() => {
     const initStyle = {
       // 控制动画结束后 隐藏的时间 所以需要比动画执行的时间长
       // transition: `opacity ${delay + 50}ms`,
       animationDuration: `${delay}ms`,
       animationName: visible ? enterAnimation : levaeAnimation,
     }
-    const classes = `transitionGroup ${className} ${flag ? "show" : "hide"} ${visible ? 'pointerEventsAll' : 'pointerEventsNone'}`
 
-    return (<div className={classes} ref={transitionGroup} style={initStyle}>{props.children}</div>)
-  }
+    const classs = `transitionGroup ${className} ${visible ? 'pointerEventsAll' : 'pointerEventsNone'}`
+
+    return (<div className={classs} ref={transitionGroup} style={initStyle}>{props.children}</div>)
+  }, [visible, delay])
 
   return render()
 }
--- static/src/components/wallpaper/wallpaper.tsx
+++ static/src/components/wallpaper/wallpaper.tsx
@@ -57,16 +57,16 @@ export default function Wallpaper(props: IProps) {
       setLoadStatus(true)
     })
   }, [wallpaper])
-
+  
   return (
     <div className={`background ${isBlur ? 'blur': ''}`}>
-      {/* <TransitionGroup
+      <TransitionGroup
         visible={loadStatus}
         enterAnimation="fadeIn"
         levaeAnimation="fadeOut" 
       >
         {loadStatus && <div className='wallpaper-image' style={{backgroundImage: `url(${wallpaper})`}}></div>}
-      </TransitionGroup> */}
+      </TransitionGroup>
       <canvas ref={canvasMain} className="none" id="canvas"></canvas>
     </div>
   )

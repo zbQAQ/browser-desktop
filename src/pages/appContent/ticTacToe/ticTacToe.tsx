7105fe047d76b834afa8d99b6196a65e795b4274
zbQAQ <1290368140@qq.com>
2023-03-13 18:11:00 +0800

    fix: build error

--- src/pages/appContent/ticTacToe/ticTacToe.tsx
+++ src/pages/appContent/ticTacToe/ticTacToe.tsx
@@ -1,128 +1,168 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from 'react';
 
-import "./tictactoe.css"
+import './ticTacToe.css';
 
-type INextStepType = 'computer' | 'player'
+type INextStepType = 'computer' | 'player';
 
-type ICheckerValueType = 'null' | 'x' | 'o'
+type ICheckerValueType = 'null' | 'x' | 'o';
 
 /**
  * 0 0 0
  * 0 0 0
  * 0 0 0
- * 
+ *
  * 横排
  * [0, 0] [0, 1] [0, 2]
  * [1, 0] [1, 1] [1, 2]
  * [2, 0] [2, 1] [2, 2]
- * 
+ *
  * 斜着
  * [0, 0] [1, 1] [2, 2]
  * [0, 2] [1, 1] [2, 0]
- * 
- * 
+ *
+ *
  * 竖排
  * [0, 0] [1, 0] [2, 0]
  * [0, 1] [1, 1] [2, 1]
  * [0, 2] [1, 2] [2, 2]
- * 
+ *
  */
 
-
 let initBoard: ICheckerValueType[][] = [
-  ["null", "null", "null"],
-  ["null", "null", "null"],
-  ["null", "null", "null"]
-]
+  ['null', 'null', 'null'],
+  ['null', 'null', 'null'],
+  ['null', 'null', 'null'],
+];
 
 const initBoardApi = {
-  changeBoardValueByIndex: (rIndex: number, cIndex: number, value: ICheckerValueType) => {
-    initBoard[rIndex][cIndex] = value
-    return initBoard
+  changeBoardValueByIndex: (
+    rIndex: number,
+    cIndex: number,
+    value: ICheckerValueType
+  ) => {
+    initBoard[rIndex][cIndex] = value;
+    return initBoard;
   },
   reset: () => {
-    initBoard = Array(["null", "null", "null"], ["null", "null", "null"], ["null", "null", "null"])
-    return initBoard
-  }
-}
+    initBoard = Array(
+      ['null', 'null', 'null'],
+      ['null', 'null', 'null'],
+      ['null', 'null', 'null']
+    );
+    return initBoard;
+  },
+};
 
 const overSituation = {
-  row1: [ [0, 0], [0, 1], [0, 2] ],
-  row2: [ [1, 0], [1, 1], [1, 2] ],
-  row3: [ [2, 0], [2, 1], [2, 2] ],
-  col1: [ [0, 0], [1, 0], [2, 0] ],
-  col2: [ [0, 1], [1, 1], [2, 1] ],
-  col3: [ [0, 2], [1, 2], [2, 2] ],
-  slash1: [ [0, 0], [1, 1], [2, 2] ],
-  slash2: [ [0, 2], [1, 1], [2, 0] ],
-}
+  row1: [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+  row2: [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+  ],
+  row3: [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  col1: [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+  ],
+  col2: [
+    [0, 1],
+    [1, 1],
+    [2, 1],
+  ],
+  col3: [
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ],
+  slash1: [
+    [0, 0],
+    [1, 1],
+    [2, 2],
+  ],
+  slash2: [
+    [0, 2],
+    [1, 1],
+    [2, 0],
+  ],
+};
 
-const FIRST_STEP: INextStepType = "player"
+const FIRST_STEP: INextStepType = 'player';
 
 export default function TicTacToe() {
-  const [nextStep, setNextStep] = useState<INextStepType>(FIRST_STEP)
-  const [checkerBoard, setCheckerBoard] = useState<ICheckerValueType[][]>(initBoard)
-  const [emptyCheckerItems, setEmptyCheckerItems] = useState<[number, number][]>([[1, 1]])
-  const [gameover, setGameover] = useState(false)
-  const [overStatus, setOverStatus] = useState('')
-  const [winner, setWinner] = useState('')
+  const [nextStep, setNextStep] = useState<INextStepType>(FIRST_STEP);
+  const [checkerBoard, setCheckerBoard] =
+    useState<ICheckerValueType[][]>(initBoard);
+  const [emptyCheckerItems, setEmptyCheckerItems] = useState<
+    [number, number][]
+  >([[1, 1]]);
+  const [gameover, setGameover] = useState(false);
+  const [overStatus, setOverStatus] = useState('');
+  const [winner, setWinner] = useState('');
 
   useEffect(() => {
-    const emptyItems = resetEmptyCheckerItems()
+    const emptyItems = resetEmptyCheckerItems();
     /** 监测游戏结束  */
-    const overSituationKey = Object.keys(overSituation)
-    let isOver = false
-    for(let i = 0; i < overSituationKey.length; i++) {
-      let firstValue = ''
-      const curSit = overSituation[overSituationKey[i]]
-      for(let j = 0; j < curSit.length; j++) {
-        const sitValue = getValueByIndex(curSit[j][0], curSit[j][1])
-        if(sitValue === 'null') {
-          isOver = false
-          break
-        }else if(firstValue === '') {
-          firstValue = sitValue
-        }else if(firstValue !== sitValue){
-          isOver = false
-          break
-        }else {
-          isOver = true
+    const overSituationKey = Object.keys(overSituation);
+    let isOver = false;
+    for (let i = 0; i < overSituationKey.length; i++) {
+      let firstValue = '';
+      const curSit = overSituation[overSituationKey[i]];
+      for (let j = 0; j < curSit.length; j++) {
+        const sitValue = getValueByIndex(curSit[j][0], curSit[j][1]);
+        if (sitValue === 'null') {
+          isOver = false;
+          break;
+        } else if (firstValue === '') {
+          firstValue = sitValue;
+        } else if (firstValue !== sitValue) {
+          isOver = false;
+          break;
+        } else {
+          isOver = true;
         }
       }
 
-      if(isOver) {
-        setGameover(true)
-        setOverStatus(overSituationKey[i])
-        setWinner(firstValue === 'o' ? 'player' : 'computer')
-        break
+      if (isOver) {
+        setGameover(true);
+        setOverStatus(overSituationKey[i]);
+        setWinner(firstValue === 'o' ? 'player' : 'computer');
+        break;
       }
     }
 
-    if(!isOver && emptyItems.length <= 0) {
-      setGameover(true)
-      setWinner('null')
+    if (!isOver && emptyItems.length <= 0) {
+      setGameover(true);
+      setWinner('null');
     }
-
-  }, [checkerBoard])
+  }, [checkerBoard]);
 
   useEffect(() => {
-    if(!gameover && nextStep === 'computer') {
+    if (!gameover && nextStep === 'computer') {
       // 棋盘还有位置并且监测到游戏还没结束
-      if(emptyCheckerItems.length > 0) {
+      if (emptyCheckerItems.length > 0) {
         //轮到电脑
-        const coordinate = randomCoordinate(emptyCheckerItems)
-        computerClickColItem(coordinate[0], coordinate[1])
-        setNextStep('player')
+        const coordinate = randomCoordinate(emptyCheckerItems);
+        computerClickColItem(coordinate[0], coordinate[1]);
+        setNextStep('player');
       }
     }
-  }, [nextStep])
+  }, [nextStep]);
 
   useEffect(() => {
     return () => {
-      restartGame()
-    }
-  }, [])
-
+      restartGame();
+    };
+  }, []);
 
   const renderBoard = () => {
     return (
@@ -131,87 +171,98 @@ export default function TicTacToe() {
           return (
             <div className="row" key={`${row}${rIndex}`}>
               {row.map((col, cIndex) => {
-                return renderColItem(col, rIndex, cIndex)
+                return renderColItem(col, rIndex, cIndex);
               })}
             </div>
-          )
+          );
         })}
       </>
-    )
-  }
-  
+    );
+  };
+
   const renderColItem = (value: string, rIndex: number, cIndex: number) => {
     return (
-      <div className="col pointer" key={`${rIndex}${cIndex}`} onClick={() => {playerClickColItem(rIndex, cIndex)}}>
+      <div
+        className="col pointer"
+        key={`${rIndex}${cIndex}`}
+        onClick={() => {
+          playerClickColItem(rIndex, cIndex);
+        }}
+      >
         {value === 'null' ? '' : value}
       </div>
-    )
-  }
+    );
+  };
 
   const resetEmptyCheckerItems = () => {
     //获取棋盘上空位的所有坐标
-    const emptyCheckerItem: [number, number][] = []
-    for(let i = 0; i < initBoard.length; i++) {
-      for(let j = 0; j < initBoard[i].length; j++) {
-        if(initBoard[i][j] === 'null') emptyCheckerItem.push([i, j]);
+    const emptyCheckerItem: [number, number][] = [];
+    for (let i = 0; i < initBoard.length; i++) {
+      for (let j = 0; j < initBoard[i].length; j++) {
+        if (initBoard[i][j] === 'null') emptyCheckerItem.push([i, j]);
       }
     }
-    setEmptyCheckerItems([...emptyCheckerItem])
-    return emptyCheckerItem
-  }
+    setEmptyCheckerItems([...emptyCheckerItem]);
+    return emptyCheckerItem;
+  };
 
   //玩家点击
   const playerClickColItem = async (rIndex: number, cIndex: number) => {
-    await changeValueByIndex(rIndex, cIndex)
-    setNextStep('computer')
-    
-  }
+    await changeValueByIndex(rIndex, cIndex);
+    setNextStep('computer');
+  };
 
   //电脑点击
   const computerClickColItem = async (rIndex: number, cIndex: number) => {
-    await changeValueByIndex(rIndex, cIndex)
-  }
-
+    await changeValueByIndex(rIndex, cIndex);
+  };
 
   /** 通过传入的坐标返回value */
-  const getValueByIndex = (rIndex: number, cIndex: number) => checkerBoard[rIndex][cIndex]
+  const getValueByIndex = (rIndex: number, cIndex: number) =>
+    checkerBoard[rIndex][cIndex];
 
   /** 通过传入的坐标改变value */
   const changeValueByIndex = (rIndex: number, cIndex: number) => {
-    return new Promise(resolve => {
-      const targetValue = nextStep === 'computer' ? 'x' : 'o'
-      if(getValueByIndex(rIndex, cIndex) === 'null') {
-        const newBoard = initBoardApi.changeBoardValueByIndex(rIndex, cIndex, targetValue)
-        setCheckerBoard([...newBoard])
-        resolve(null)
+    return new Promise((resolve) => {
+      const targetValue = nextStep === 'computer' ? 'x' : 'o';
+      if (getValueByIndex(rIndex, cIndex) === 'null') {
+        const newBoard = initBoardApi.changeBoardValueByIndex(
+          rIndex,
+          cIndex,
+          targetValue
+        );
+        setCheckerBoard([...newBoard]);
+        resolve(null);
       }
-    })
-  }
+    });
+  };
 
-  /** 
+  /**
    * 轮到电脑下的时候 从所有空的坐标中 随机坐标
    * emptyItems 棋盘上的空位
    */
-  const randomCoordinate = (emptyItems: [number, number][]): [number, number] => {
-    if(emptyItems.length === 0) {
-      return emptyItems[0]
+  const randomCoordinate = (
+    emptyItems: [number, number][]
+  ): [number, number] => {
+    if (emptyItems.length === 0) {
+      return emptyItems[0];
     }
-    const randomIndex = Math.floor(Math.random() * emptyItems.length)
-    const coordinate = emptyItems[randomIndex]
-    if(getValueByIndex(coordinate[0], coordinate[1]) !== 'null') {
-      return randomCoordinate(emptyItems)
+    const randomIndex = Math.floor(Math.random() * emptyItems.length);
+    const coordinate = emptyItems[randomIndex];
+    if (getValueByIndex(coordinate[0], coordinate[1]) !== 'null') {
+      return randomCoordinate(emptyItems);
     }
-    return coordinate
-  }
+    return coordinate;
+  };
 
   const restartGame = () => {
-    setNextStep(FIRST_STEP)
-    setCheckerBoard(initBoardApi.reset())
-    setEmptyCheckerItems([[1, 1]])
-    setGameover(false)
-    setOverStatus('')
-    setWinner('')
-  }
+    setNextStep(FIRST_STEP);
+    setCheckerBoard(initBoardApi.reset());
+    setEmptyCheckerItems([[1, 1]]);
+    setGameover(false);
+    setOverStatus('');
+    setWinner('');
+  };
 
   return (
     <div className="ticTacToeContainer">
@@ -225,10 +276,15 @@ export default function TicTacToe() {
         <div className={`overLine ${overStatus !== '' && overStatus}`}></div>
       </div>
       <div className={`overMessage textCenter ${gameover ? '' : 'none'}`}>
-        <p className="mb10">{winner !== 'null' ? `${winner === 'player' ? '你': '电脑'}赢了！`: '和局'}</p>
-        <p className="pointer restartBtn" onClick={restartGame}>要不要再来一局</p>
+        <p className="mb10">
+          {winner !== 'null'
+            ? `${winner === 'player' ? '你' : '电脑'}赢了！`
+            : '和局'}
+        </p>
+        <p className="pointer restartBtn" onClick={restartGame}>
+          要不要再来一局
+        </p>
       </div>
-
     </div>
-  )
+  );
 }
--- src/pages/appContent/waterfall/waterfall.tsx
+++ src/pages/appContent/waterfall/waterfall.tsx
@@ -1,239 +1,261 @@
-import React, { useEffect, useRef, useState } from "react"
-import { randomNum } from "@/util/common"
+import React, { useEffect, useRef, useState } from 'react';
+import { randomNum } from '@/util/common';
 
-import "./Waterfall.less"
+import './waterfall.less';
 
 // 等宽 | 等高
-type IEqualType = 'width' | 'height' 
+type IEqualType = 'width' | 'height';
 
 interface IDataModel {
-  text: number
-  width: number
-  height: number
-  backgroundColor: string
-  top?: number
-  left?: number
-  styles: Record<string, any>
+  text: number;
+  width: number;
+  height: number;
+  backgroundColor: string;
+  top?: number;
+  left?: number;
+  styles: Record<string, any>;
 }
 
 // 随机数据
 const randomData = (len: number, type = 'width') => {
-  const res: IDataModel[] = []
-  for(let i = 0; i < len; i++) {
+  const res: IDataModel[] = [];
+  for (let i = 0; i < len; i++) {
     res.push({
       text: i + 1,
       width: type === 'width' ? 200 : randomNum(200, 300),
       height: type === 'height' ? 200 : randomNum(260, 500),
-      backgroundColor: `rgba(${randomNum(0, 255)}, ${randomNum(0, 255)}, ${randomNum(0, 255)}, 0.5)`,
-      styles: {}
-    })
+      backgroundColor: `rgba(${randomNum(0, 255)}, ${randomNum(
+        0,
+        255
+      )}, ${randomNum(0, 255)}, 0.5)`,
+      styles: {},
+    });
   }
 
   return res;
-}
+};
 
 // 初始化 数据地图信息
 const constructorList = (len: number): any[] => {
-  const res: any[] = []
-  if(len <= 0) return res
-  let i = 0
-  while(i < len) {
-    res.push([])
-    i++
+  const res: any[] = [];
+  if (len <= 0) return res;
+  let i = 0;
+  while (i < len) {
+    res.push([]);
+    i++;
   }
-  return res
-}
+  return res;
+};
 
 // 获取当前数据 所在列的下标
 const getColumnIndex = (index: number, columnNum: number) => {
-  return index % columnNum
-}
+  return index % columnNum;
+};
 
 // 根据当前 x y 获取 top 和 left
-const getPosition = (map: IDataModel[][], columnNum: number, rowIndex: number, spacing: number) => {
-
-  let top = 0
-  let left = 0
-  if(rowIndex > 0) {
-    let grand = map[columnNum].slice(0, rowIndex)
-    let h = grand.reduce((total: number, next: any) => total + next.height, 0)
-    let s = grand.length * spacing
-    top = h + s
+const getPosition = (
+  map: IDataModel[][],
+  columnNum: number,
+  rowIndex: number,
+  spacing: number
+) => {
+  let top = 0;
+  let left = 0;
+  if (rowIndex > 0) {
+    let grand = map[columnNum].slice(0, rowIndex);
+    let h = grand.reduce((total: number, next: any) => total + next.height, 0);
+    let s = grand.length * spacing;
+    top = h + s;
   }
-  if(columnNum > 0) {
-    let grand = map.slice(0, columnNum)
-    let w = grand.reduce((total, next) => total + next[0].width, 0)
-    let s = grand.length * spacing
-    left = w + s
+  if (columnNum > 0) {
+    let grand = map.slice(0, columnNum);
+    let w = grand.reduce((total, next) => total + next[0].width, 0);
+    let s = grand.length * spacing;
+    left = w + s;
   }
-  return { top, left }
-}
+  return { top, left };
+};
 
 // 获取高度最高的列高和
 const getMaxHeight = (map: IDataModel[][]) => {
-  const heightArr = map.map(col => (col[col.length - 1].top || 0) + col[col.length - 1].height)
-  return Math.max(...heightArr)
-}
+  const heightArr = map.map(
+    (col) => (col[col.length - 1].top || 0) + col[col.length - 1].height
+  );
+  return Math.max(...heightArr);
+};
 
-const DATA_NUM = 30
+const DATA_NUM = 30;
 
 function Waterfall() {
-
   const btnOption = [
-    {text: "刷新数据", key: "refresh", onclick: () => setData(randomData(DATA_NUM, equalType))},
-    {text: "等宽布局", key: "width", onclick: () => setEqualType('width')},
-    {text: "等高布局", key: "height", onclick: () => setEqualType('height')},
-  ]
+    {
+      text: '刷新数据',
+      key: 'refresh',
+      onclick: () => setData(randomData(DATA_NUM, equalType)),
+    },
+    { text: '等宽布局', key: 'width', onclick: () => setEqualType('width') },
+    { text: '等高布局', key: 'height', onclick: () => setEqualType('height') },
+  ];
 
-  
   // 布局类型 等宽或等高
-  const [equalType, setEqualType] = useState<IEqualType>("width")
+  const [equalType, setEqualType] = useState<IEqualType>('width');
   // 布局数据
-  const [data, setData] = useState(randomData(DATA_NUM, equalType))
+  const [data, setData] = useState(randomData(DATA_NUM, equalType));
   // 整体瀑布流容器 宽度
-  const [flowContainerW, setFlowContainerW] = useState(0)
+  const [flowContainerW, setFlowContainerW] = useState(0);
   // 整体瀑布流容器 高度
-  const [flowContainerH, setFlowContainerH] = useState(0)
+  const [flowContainerH, setFlowContainerH] = useState(0);
   // 整体瀑布流容器
-  const flowContent = useRef<HTMLDivElement>(null)
+  const flowContent = useRef<HTMLDivElement>(null);
   // 布局信息 列数、列间距
-  const [grid, setGrid] = useState({ columnNum: 0, spacing: 0 })
+  const [grid, setGrid] = useState({ columnNum: 0, spacing: 0 });
   // 最终渲染的列表
-  const [list, setList] = useState<IDataModel[]>([])
+  const [list, setList] = useState<IDataModel[]>([]);
 
   useEffect(() => {
-    setData(randomData(DATA_NUM, equalType))
-  }, [equalType])
+    setData(randomData(DATA_NUM, equalType));
+  }, [equalType]);
 
   useEffect(() => {
-    if(flowContent.current) {
-      setFlowContainerW(flowContent.current.offsetWidth)
+    if (flowContent.current) {
+      setFlowContainerW(flowContent.current.offsetWidth);
       // 等宽  根据容器宽度获取列数和间距
-      const columnNum = Math.floor(flowContent.current.offsetWidth / data[0].width)
-      const spacing = (flowContent.current.offsetWidth % data[0].width) / (columnNum - 1)
+      const columnNum = Math.floor(
+        flowContent.current.offsetWidth / data[0].width
+      );
+      const spacing =
+        (flowContent.current.offsetWidth % data[0].width) / (columnNum - 1);
 
-      setGrid({ columnNum, spacing })
+      setGrid({ columnNum, spacing });
     }
-  }, [flowContent, data])
+  }, [flowContent, data]);
 
   useEffect(() => {
-    if(grid.columnNum > 0) {
-      getFlowItem(data, equalType)
+    if (grid.columnNum > 0) {
+      getFlowItem(data, equalType);
     }
-  }, [grid, data])
+  }, [grid, data]);
 
   const getFlowItem = (data: IDataModel[], type: 'width' | 'height') => {
-
-    if(type === 'width') {
+    if (type === 'width') {
       // 等宽
-      const { columnNum, spacing } = grid
-      const listInfoMap = constructorList(columnNum)
-      for(let i = 0; i < data.length; i++) {
-        const cur = data[i]
-        listInfoMap[getColumnIndex(i, columnNum)].push(cur)
+      const { columnNum, spacing } = grid;
+      const listInfoMap = constructorList(columnNum);
+      for (let i = 0; i < data.length; i++) {
+        const cur = data[i];
+        listInfoMap[getColumnIndex(i, columnNum)].push(cur);
       }
-  
-      for(let i = 0; i < columnNum; i++) {
-        const columnData = listInfoMap[i]
-        for(let j = 0; j < columnData.length; j++) {
-          const curItem = columnData[j]
-          const { top, left } = getPosition(listInfoMap, i, j, spacing)
-          curItem["top"] = top
-          curItem["left"] = left
-          curItem["styles"] = {
+
+      for (let i = 0; i < columnNum; i++) {
+        const columnData = listInfoMap[i];
+        for (let j = 0; j < columnData.length; j++) {
+          const curItem = columnData[j];
+          const { top, left } = getPosition(listInfoMap, i, j, spacing);
+          curItem['top'] = top;
+          curItem['left'] = left;
+          curItem['styles'] = {
             width: curItem.width,
             height: curItem.height,
             backgroundColor: curItem.backgroundColor,
             top: curItem.top,
             left: curItem.left,
-          }
+          };
         }
       }
-      setFlowContainerH(getMaxHeight(listInfoMap))
-      setList(listInfoMap.flat())
-    } else if(type === 'height') {
+      setFlowContainerH(getMaxHeight(listInfoMap));
+      setList(listInfoMap.flat());
+    } else if (type === 'height') {
       // 等高
-      const height = data[0].height
+      const height = data[0].height;
       // 容器宽度
-      const clientWidth = flowContainerW
+      const clientWidth = flowContainerW;
       // 等高布局行间距固定
-      const rowSpacing = 20
+      const rowSpacing = 20;
       // 当前是 第几行
-      let rowCount = 0
+      let rowCount = 0;
 
-      let list = []
-      
-      let lastItemsH = 0
-      let lastItemsW = 0
-      for(let i = 0; i < data.length; i++) {
-        const curItem = data[i]
-        const nextItem = data[i + 1]
-        if(i === 0) {
-          curItem["top"] = 0
-          curItem["left"] = 0
+      let list = [];
+
+      let lastItemsH = 0;
+      let lastItemsW = 0;
+      for (let i = 0; i < data.length; i++) {
+        const curItem = data[i];
+        const nextItem = data[i + 1];
+        if (i === 0) {
+          curItem['top'] = 0;
+          curItem['left'] = 0;
         } else {
-          curItem["top"] = lastItemsH
-          curItem["left"] = lastItemsW
+          curItem['top'] = lastItemsH;
+          curItem['left'] = lastItemsW;
         }
 
-        curItem["styles"] = {
+        curItem['styles'] = {
           width: curItem.width,
           height: height,
           backgroundColor: curItem.backgroundColor,
           top: curItem.top,
           left: curItem.left,
-        }
+        };
 
-        lastItemsH = (height + rowSpacing) * rowCount
-        lastItemsW += curItem.width
+        lastItemsH = (height + rowSpacing) * rowCount;
+        lastItemsW += curItem.width;
 
-        if(!!list[rowCount]) {
-          list[rowCount].push(curItem)
+        if (!!list[rowCount]) {
+          list[rowCount].push(curItem);
         } else {
-          list[rowCount] = [curItem]
+          list[rowCount] = [curItem];
         }
 
-        if(lastItemsW + (nextItem ? nextItem.width : 0) > clientWidth) {
-          rowCount += 1
-          lastItemsH = height * rowCount + rowCount * rowSpacing
-          lastItemsW = 0
+        if (lastItemsW + (nextItem ? nextItem.width : 0) > clientWidth) {
+          rowCount += 1;
+          lastItemsH = height * rowCount + rowCount * rowSpacing;
+          lastItemsW = 0;
         }
       }
 
-      for(let i = 0; i < list.length; i++) {
-        const row = list[i]
-        const rowLen = row.length
-        const colSpacing = (clientWidth - row.reduce((total, next) => total + next.width, 0)) / (rowLen - 1)
-        for(let j = 1; j < rowLen; j++) {
-          row[j]["styles"].left = (row[j]["styles"].left || 0) + colSpacing * j
+      for (let i = 0; i < list.length; i++) {
+        const row = list[i];
+        const rowLen = row.length;
+        const colSpacing =
+          (clientWidth - row.reduce((total, next) => total + next.width, 0)) /
+          (rowLen - 1);
+        for (let j = 1; j < rowLen; j++) {
+          row[j]['styles'].left = (row[j]['styles'].left || 0) + colSpacing * j;
         }
       }
-      
-      setFlowContainerH( height * (rowCount + 1) + rowSpacing * rowCount)
-      setList(list.flat())
+
+      setFlowContainerH(height * (rowCount + 1) + rowSpacing * rowCount);
+      setList(list.flat());
     }
-
-  }
-  
+  };
 
   return (
     <div className="waterfall-container">
       <div className="flow-options fontWhite mb20 pb10">
         {btnOption.map((v, index) => (
-          <span key={index} className={`pointer ${v.key === equalType ? 'active' : ''}`} onClick={v.onclick}>{v.text}</span>
+          <span
+            key={index}
+            className={`pointer ${v.key === equalType ? 'active' : ''}`}
+            onClick={v.onclick}
+          >
+            {v.text}
+          </span>
         ))}
       </div>
-      <div className="flow-content" ref={flowContent} style={{height: flowContainerH}}>
-        {list.length > 0 && (
-          list.map(v => (
+      <div
+        className="flow-content"
+        ref={flowContent}
+        style={{ height: flowContainerH }}
+      >
+        {list.length > 0 &&
+          list.map((v) => (
             <div key={v.text} className="flow-item textCenter" style={v.styles}>
               <p className="fontWhite">{v.text}</p>
             </div>
-          ))
-        )}
+          ))}
       </div>
-    </div>  
-  )
+    </div>
+  );
 }
 
 export default Waterfall;

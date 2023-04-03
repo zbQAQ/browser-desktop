e17969a939e9cd39e78ad8a4e7f82097d17132dd
zbQAQ <1290368140@qq.com>
2023-04-03 12:33:08 +0800

    fix: build error

--- src/pages/appContent/aircraftWar/index.tsx
+++ src/pages/appContent/aircraftWar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import GameContainer from './components/gameContainer/gameContainer';
 import { AircraftWarProvider } from '@/context/aircraftWarProvider';
 
